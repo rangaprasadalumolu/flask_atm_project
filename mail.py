7028cbad9fbf57771c76172adf1ee5cecb524465
rangaprasadalumolu <alumolurangaprasad1@gmail.com>
2026-06-08 12:27:07 +0530

    updated mail.py

--- mail.py
+++ mail.py
@@ -6,17 +6,30 @@ EMAIL = os.getenv("EMAIL_USER")
 PASSWORD = os.getenv("EMAIL_PASS")
 
 def send_email(to_email, subject, body):
-    msg = MIMEText(body)
-    msg["Subject"] = subject
-    msg["From"] = EMAIL
-    msg["To"] = to_email
 
     try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        msg = MIMEText(body)
+
+        msg["Subject"] = subject
+        msg["From"] = EMAIL
+        msg["To"] = to_email
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, timeout=30) as server:
             server.login(EMAIL, PASSWORD)
             server.send_message(msg)
 
         print("Email sent successfully")
 
     except Exception as e:
-        print("Email Error:", e)
+        print("===================================")
+        print("EMAIL ERROR")
+        print(str(e))
+        print("To:", to_email)
+        print("Subject:", subject)
+        print("Body:", body)
+        print("===================================")
+
+        # Prevent application crash
+        return False
+
+    return True
