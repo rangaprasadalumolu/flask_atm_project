14da0098fe3d2f1c604969a3d0f82536c1aca55c
rangaprasadalumolu <alumolurangaprasad1@gmail.com>
2026-06-08 12:06:15 +0530

    updated mail.py

--- mail.py
+++ mail.py
@@ -2,27 +2,19 @@ import smtplib
 from email.mime.text import MIMEText
 import os
 
-EMAIL = os.environ.get("EMAIL_USER")
-PASSWORD = os.environ.get("EMAIL_PASS")
+EMAIL = os.getenv("EMAIL_USER")
+PASSWORD = os.getenv("EMAIL_PASS")
 
 def send_email(to_email, subject, body):
-
     msg = MIMEText(body)
-
-    msg['Subject'] = subject
-    msg['From'] = EMAIL
-    msg['To'] = to_email
+    msg["Subject"] = subject
+    msg["From"] = EMAIL
+    msg["To"] = to_email
 
     try:
-        server = smtplib.SMTP("smtp.gmail.com", 587, timeout=30)
-
-        server.starttls()
-
-        server.login(EMAIL, PASSWORD)
-
-        server.sendmail(EMAIL, to_email, msg.as_string())
-
-        server.quit()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(EMAIL, PASSWORD)
+            server.send_message(msg)
 
         print("Email sent successfully")
 
